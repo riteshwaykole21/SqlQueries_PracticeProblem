c63fde21b3b81b94cbe33bd4c28772bfb54c471c
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-30 10:31:29 +0530

    [Ritesh]add :sales People Full Information

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,10 +1,19 @@
-create Database TestDB
-create table customer
+create Database SalesPeople
+create Table salesman
 (
-  "ID" int Primary Key,
-  "Name" varchar(50) Not Null,
-  "Age" int Not Null,
-  "City" char(50),
-  "Salary" numeric
-)
-select * From customer
+   SalesManID int,
+   Name varchar(50),
+   City Varchar(50),
+   Commission int
+);
+select * From salesman
+insert into salesman
+(SalesManID, Name, City, Commission)
+Values
+(5001 ,'james Hoog', 'new York',15),
+(5002 ,'Nail Knite', 'paris',13),
+(5005 ,'pit Alex', 'new',12),
+(5006 ,' Hoog', 'London',15),
+(5007 ,'james', 'new Holland',11),
+(5003 ,'jamesHoog', 'York',17);
+select * From salesman
